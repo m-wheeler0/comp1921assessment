7cbd74b35ed2994972c176e0f97e8c160eb26a4b
m-wheeler0 <146672950+m-wheeler0@users.noreply.github.com>
2024-02-29 12:25:01 +0000

    Included modules and defined structs

--- mazeGame.c
+++ mazeGame.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
 
+/*Each maze map will be stored within this struct, showing rows, columns allowing for easier
+iteration over each character.*/
+
+typedef struct
+{
+    int rows;
+    int columns;
+    FILE *mazeFile;
+}maze_map;
+
+int main(){
+    return 0;
+}
